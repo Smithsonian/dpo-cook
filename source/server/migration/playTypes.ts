5f3f3921f20cec28b266498acdbc20cae2545628
Ralph Wiedemeier <ralph@framefactory.ch>
2019-09-03 09:41:26 +0200

    migration recipes, work in progress

---
+++ source/server/migration/playTypes.ts
@@ -0,0 +1,275 @@
+/**
+ * 3D Foundation Project
+ * Copyright 2019 Smithsonian Institution
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export interface IPlayBoxInfo
+{
+    bake: IPlayBake;
+    config: IPlayConfig;
+    descriptor: IPlayDescriptor;
+    payload: IPlayPayload;
+}
+
+export interface IPlayBake
+{
+    box: {
+        name: string;
+        description: string;
+        tags: { [id:string]: string },
+        mount: { [id:string]: string}
+    },
+    assets: { [id:string]: IPlayAsset }
+}
+
+export interface IPlayAsset
+{
+    name: string;
+    size: number;
+    type: string;
+    files: { [id:string]: string }
+}
+
+export interface IPlayDescriptor
+{
+    name: string;
+    units: string;
+    transform: number[];
+    parts: IPlayPart[];
+}
+
+export interface IPlayPart
+{
+    name: string;
+    files: {
+        mesh: string;
+        diffuse?: string;
+        normal?: string;
+        occlusion?: string;
+    }
+}
+
+export interface IPlayConfig
+{
+    "Material - Curator Settings": any;
+    "Background - Curator Settings": any;
+    "Grid - Curator Settings": any;
+    "Measure - Curator Settings": any;
+    "Lighting - Curator Settings": any;
+    "Curator Tools": any;
+    "Viewcube and Grid for thumbnail": any;
+    "Title - Curator Settings": any;
+    "Annotation - Curator Settings": any;
+    "Camera - Curator Settings": any;
+    "Model Rotation - Curator Settings": any;
+
+    "Sidebar": { URL: string };
+    "Default Sidebar": { URL: string };
+    "Curator Sidebar": { URL: string };
+}
+
+export interface IPlayPayload
+{
+    name: string;
+    message: {
+        isCustomScene: boolean;
+        boxId: string;
+        pubName: string;
+        pubUrl: string;
+        pubThumb: string;
+        pubPreview: string;
+        tours: IPlayTour[];
+        annotations: IPlayAnnotations[];
+    }
+}
+
+export interface IPlayAnnotations
+{
+    name: string;
+    description: string;
+    annotations: IPlayAnnotation[]
+}
+
+export interface IPlayAnnotation
+{
+    "Transform.Position": number[];
+    "Transform.Rotation": number[];
+    "Title": string;
+    "Body": string;
+    "Link": string;
+    "labels": string;
+    "Stem.On": boolean;
+    "Stem.Length": number;
+    "Stem.Width": number;
+    "Stem.Rot.X": number;
+    "Stem.Rot.Y": number;
+    "Stem.Color": number[]; // RGB float
+    "index": number;
+    "Opacity.Open": number;
+    "Opacity.Closed": number;
+    "Fade": boolean;
+    "Fade.Near": number;
+    "Fade.Far": number;
+    "TextSize.Header": number;
+    "TextSize.Body": number;
+    "TextSize.Link": number;
+    "Annotation.Width": number;
+}
+
+export interface IPlayTour
+{
+    name: string;
+    description: string;
+    snapshots: IPlaySnapshot[];
+}
+
+export interface IPlaySnapshot
+{
+    name: string;
+    data: IPlaySnapshotData;
+    transition: {
+        duration: number;
+        switch: number;
+        curve: number;
+    }
+}
+
+export interface IPlaySnapshotData
+{
+    "Material Store": {
+        "Material.Roughness": number;
+        "Material.Metalness": number;
+        "Material.Exposure": number;
+        "Material.Gamma": number;
+        "Material.Occlusion": number;
+        "Material.Regular.Opacity": number;
+        "Material.Wireframe.Opacity": number;
+        "Material.X-ray.Opacity": number;
+        "Material.Regular.Color": number[];
+        "Material.Wireframe.Color": number[];
+        "Material.X-ray.Color": number[];
+        "Material.PhotoTexture": number;
+        "Material.Menu": number;
+    };
+    "Lighting Store": {
+        "Environment.Preset": number;
+        "Lighting.Pointlight.On": boolean;
+        "Lighting.Pointlight.Manip.On": boolean;
+        "Lighting.Environment.Exposure": number;
+        "Lighting.Pointlight.Position": number[];
+        "Lighting.Pointlight.Intensity": number;
+        "Lighting.Pointlight.Color": number[];
+    };
+    "Background Store": {
+        "Background.Menu": number;
+        "Background.Top.Color": number[];
+        "Background.Bottom.Color": number[];
+    };
+    "Grid Store": {
+        "Grid.On": boolean;
+        "Grid.Units": number;
+        "Grid.Menu": number;
+        "Grid.Single.Menu": number;
+        "Grid.Single.On": boolean;
+        "Grid.Triple.On": boolean;
+        "Grid.Size": string;
+        "Grid.Opacity": number;
+        "Grid.Primary.Color": number[];
+        "Grid.Secondary.Color": number[];
+    };
+    "Camera Store": {
+        "WhichCam": number;
+        "WhichOrientation": number;
+        "Camera.Orientation": number[]; // Vector 3
+        "Camera.Offset": number[];      // Vector 3
+        "Camera.Center": number[];      // Vector 3
+        "Camera.Distance": number,
+        "Camera.Transform": number[];   // Matrix 4
+        "Camera.FOV": number;           // Degrees
+        "Camera.Orthographic": boolean;
+    };
+    "Measure Store": {
+        "Measure.Thumbtack.On": boolean;
+        "Measure.On": boolean;
+        "Measure.Units": number;
+        "Measure.Distance": number;
+        "Measure.Point0.Position": number[];  // Vector 3
+        "Measure.Point0.Rotation": number[];  // Vector 3
+        "Measure.Point0.Scale": number[];     // Vector 3
+        "Measure.Point1.Position": number[];  // Vector 3
+        "Measure.Point1.Rotation": number[];  // Vector 3
+        "Measure.Point1.Scale": number[];     // Vector 3
+        "Measure.Line.Position": number[];    // Vector 3
+        "Measure.Line.Rotation": number[];    // Vector 3
+        "Measure.Line.Scale": number[];       // Vector 3
+        "Measure.Point0.Transform": number[]; // Matrix 4
+        "Measure.Point1.Transform": number[]; // Matrix 4
+        "Measure.Line.Transform": number[];   // Matrix 4
+    };
+    "Annotation Label Store": {
+        "Annotation.VisibleLabels": string;
+    };
+    "Layout Store": {
+        "Layout.DoubleView.On": boolean;
+        "Layout.Menu": number;
+        "Layout.Merge.On": boolean;
+    };
+    "Ortho Store": {
+        "Ortho.F.On": boolean;
+        "Ortho.R.On": boolean;
+        "Ortho.BK.On": boolean;
+        "Ortho.L.On": boolean;
+        "Ortho.T.On": boolean;
+        "Ortho.BM.On": boolean;
+    };
+    "Annotation Store": {
+        "Annotation.On": boolean;
+        "Annotation.OneLabel": boolean;
+        "Annotation.WhichOpen": number; // ID?
+    };
+    "Sidebar Store": {
+        "Sidebar.On": boolean;
+        "Sidebar.URL": string;
+    };
+    "Section Store": {
+        "Section.Manip.Position": number[]; // Vector 3
+        "Section.Manip.Rotation": number[]; // Vector 3
+        "Section.CutPlane.Position": number[]; // Vector 3
+        "Section.CutPlane.Rotation": number[]; // Vector 3
+        "Section.On": boolean;
+        "Section.Manip.On": boolean;
+    };
+    "COR Store": {
+        "COR.On": boolean;
+        "COR.Position": number[]; // Vector 3
+    };
+    "Model Space Store": {
+        "ModelSpace.Position": number[]; // Vector 3
+        "ModelSpace.Rotation": number[]; // Vector 3
+        "ModelSpace.Scale": number[]; // Vector 3
+        "ModelSpace.Transform": number[]; // Matrix 4
+        "ModelSpace.Units": string; // "millimeter"
+    };
+
+    "Background Button Store": {};
+    "Sidebar Button Store": {};
+    "Annotation Button Store": {};
+    "Material Button Store": {};
+    "Grid Button Store": {};
+    "Lighting Button Store": {};
+    "Layout Button Store": {};
+    "Measure Button Store": {};
+    "Ortho Button Store": {};
+}
--- source/server/tasks/MigratePlayTask.ts
+++ source/server/tasks/MigratePlayTask.ts
@@ -15,14 +15,20 @@
  * limitations under the License.
  */
 
-import * as fs from "fs";
-import { URL } from "url";
+import { Parser, DomHandler, DomUtils } from "htmlparser2";
+
 import fetch from "../utils/fetch";
 
 import Job from "../app/Job";
 import Task, { ITaskParameters } from "../app/Task";
 
-import { IPlayBake, IPlayPayloadInfo } from "../utils/playTypes";
+import {
+    IPlayBoxInfo,
+    IPlayBake,
+    IPlayPayload,
+    IPlayConfig,
+    IPlayDescriptor
+} from "../migration/playTypes";
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -61,8 +67,12 @@ export default class MigratePlayTask extends Task
     static readonly parameterValidator =
         Task.jsonValidator.compile(MigratePlayTask.parameterSchema);
 
-    static readonly playDataUrl = "https://legacy.3d.si.edu";
-    static readonly playPayloadLocation = "https://3d.si.edu/sites/default/files/box_payloads/";
+    protected static readonly drupalBaseUrl = "https://3d.si.edu";
+    protected static readonly payloadBaseUrl = "https://3d.si.edu/sites/default/files/box_payloads";
+    protected static readonly cdnBaseUrl = "https://d39fxlie76wg71.cloudfront.net";
+
+    protected parameters: IMigratePlayTaskParameters;
+
 
     constructor(params: IMigratePlayTaskParameters, context: Job)
     {
@@ -71,55 +81,200 @@ export default class MigratePlayTask extends Task
 
     async run(): Promise<void>
     {
-        const params = this.parameters as IMigratePlayTaskParameters;
-        const boxId = params.boxId;
-        const baseName = params.baseName || "";
-        const resultFiles: any = this.result.files = {};
+        this.result.files = {};
 
-        const payloadURL = `${MigratePlayTask.playPayloadLocation}${boxId}_payload.json`;
-        const payload = await fetch.json(payloadURL, "GET") as IPlayPayloadInfo;
+        const playBoxId = this.parameters.boxId;
 
-        resultFiles.payload = `${baseName}payload.json`;
-        await this.writeFile(resultFiles.payload, JSON.stringify(payload, null, 2));
+        const payload = await this.fetchPayload(playBoxId);
+        const { bake, config, descriptor } = await this.fetchAssets(playBoxId);
+
+        const infoContent: IPlayBoxInfo = {
+            payload,
+            bake,
+            config,
+            descriptor
+        };
+
+        const infoFileName = this.result.files["info.json"] = "info.json";
+        await this.writeFile(infoFileName, JSON.stringify(infoContent, null, 2));
+
+        await this.fetchArticles(infoContent);
+    }
+
+    /**
+     * Fetches payload.json and the associated thumbnail and preview images.
+     * @param boxId The Play box ID.
+     */
+    protected async fetchPayload(boxId: string): Promise<IPlayPayload>
+    {
+        const payloadUrl = `${MigratePlayTask.payloadBaseUrl}/${boxId}_payload.json`;
+
+        this.logTaskEvent("debug", `fetching ${payloadUrl}`);
+        const payloadContent = await fetch.json(payloadUrl, "GET") as IPlayPayload;
+        const payloadFileName = this.result.files["payload.json"] = "payload.json";
+        await this.writeFile(payloadFileName, JSON.stringify(payloadContent, null, 2));
 
         // fetch and write thumbnail image
-        const thumbImage = await fetch.buffer(payload.message.pubThumb, "GET");
-        resultFiles.thumbImage = `${baseName}image-thumb.jpg`;
-        await this.writeFile(resultFiles.thumbImage, Buffer.from(thumbImage));
+        this.logTaskEvent("debug", `fetching ${payloadContent.message.pubThumb}`);
+        const thumbImage = await fetch.buffer(payloadContent.message.pubThumb, "GET");
+        const thumbFileName = this.result.files["image-thumb.jpg"] = "image-thumb.jpg";
+        await this.writeFile(thumbFileName, Buffer.from(thumbImage));
 
         // fetch and write preview image
-        const previewImage = await fetch.buffer(payload.message.pubPreview, "GET");
-        resultFiles.previewImage = `${baseName}image-preview.jpg`;
-        await this.writeFile(resultFiles.previewImage, Buffer.from(previewImage));
+        this.logTaskEvent("debug", `fetching ${payloadContent.message.pubPreview}`);
+        const previewImage = await fetch.buffer(payloadContent.message.pubPreview, "GET");
+        const previewFileName = this.result.files["image-preview.jpg"] = "image-preview";
+        await this.writeFile(previewFileName, Buffer.from(previewImage));
 
-        // compose box base URL
-        const parts = payload.message.pubThumb.split("/");
-        const boxesIndex = parts.indexOf("boxes");
-        const cdnBaseURL = parts.slice(0, boxesIndex).join("/") + "/";
-        const boxBaseURL = cdnBaseURL + `boxes/${boxId}/`;
+        return payloadContent;
+    }
+
+    /**
+     * Fetches the bake.json asset map from CDN, then fetches all assets (original files).
+     * Returns the content of the config.json asset.
+     * @param boxId The Play box ID.
+     */
+    protected async fetchAssets(boxId: string): Promise<Partial<IPlayBoxInfo>>
+    {
+        const cdnBaseUrl = MigratePlayTask.cdnBaseUrl;
+        const boxBaseUrl = `${cdnBaseUrl}/boxes/${boxId}/`;
 
         // fetch and write bake.json
-        const bake = await fetch.json(boxBaseURL + "bake.json", "GET") as IPlayBake;
-        resultFiles.bake = `${baseName}bake.json`;
-        await this.writeFile(resultFiles.bake, JSON.stringify(bake, null, 2));
+        const bakeUrl = boxBaseUrl + "bake.json";
+        this.logTaskEvent("debug", `fetching 'bake.json' from ${bakeUrl}`);
+        const bakeContent = await fetch.json(bakeUrl, "GET") as IPlayBake;
+        const bakeFileName = this.result.files["bake.json"] = "bake.json";
+        await this.writeFile(bakeFileName, JSON.stringify(bakeContent, null, 2));
+
+        let configContent: IPlayConfig = null;
+        let descriptorContent: IPlayDescriptor = null;
 
         // fetch and write all assets
-        const assetKeys = Object.keys(bake.assets);
-        const promises = assetKeys.map(key => {
-            const asset = bake.assets[key];
-            const assetURL = `${cdnBaseURL}${asset.files["original"]}`;
-            const fileName = `${baseName}${asset.name}`;
-            resultFiles[asset.name] = fileName;
+        const assetPaths = Object.keys(bakeContent.assets);
+        const fetchAssets = assetPaths.map(assetPath => {
+            const asset = bakeContent.assets[assetPath];
+            const assetUrl = `${cdnBaseUrl}/${asset.files["original"]}`;
+            const assetFileName = this.result.files[asset.name] = asset.name;
+
+            this.logTaskEvent("debug", `fetching asset '${assetFileName}' from ${assetUrl}`);
 
             if (asset.type === "json") {
-                return fetch.json(assetURL, "GET").then(data => this.writeFile(fileName, JSON.stringify(data, null, 2)));
+                return fetch.json(assetUrl, "GET").then(data => {
+                    if (asset.name === "config.json") {
+                        configContent = data;
+                    }
+                    if (asset.name === "descriptor.json") {
+                        descriptorContent = data;
+                    }
+
+                    return this.writeFile(assetFileName, JSON.stringify(data, null, 2))
+                });
             }
             else {
-                return fetch.buffer(assetURL, "GET").then(data => this.writeFile(fileName, Buffer.from(data)));
+                return fetch.buffer(assetUrl, "GET").then(data => this.writeFile(assetFileName, Buffer.from(data)));
             }
         });
 
-        await Promise.all(promises);
+        await Promise.all(fetchAssets);
 
+        return {
+            bake: bakeContent,
+            config: configContent,
+            descriptor: descriptorContent
+        };
+    }
+
+    protected async fetchArticles(info: IPlayBoxInfo): Promise<any>
+    {
+        let articleIndex = 0;
+        const articleUrls: { [id:string]: number } = {};
+
+        // default article
+        const url = info.config["Default Sidebar"].URL;
+        if (url && !articleUrls[url]) {
+            articleUrls[url] = articleIndex++;
+        }
+
+        // tour articles
+        const tours = info.payload.message.tours;
+        tours.forEach(tour => {
+            tour.snapshots.forEach(snapshot => {
+                const sidebarUrl = snapshot.data["Sidebar Store"]["Sidebar.URL"];
+                if (sidebarUrl && !articleUrls[sidebarUrl]) {
+                    articleUrls[sidebarUrl] = articleIndex++;
+                }
+            })
+        });
+
+        const urls = Object.keys(articleUrls);
+        return Promise.all(urls.map(url => this.fetchArticle(url, articleUrls[url])));
+    }
+
+    protected async fetchArticle(url: string, index: number): Promise<any>
+    {
+        const pageHtml = await fetch.text(url, "GET");
+        const handler = new DomHandler();
+        const parser = new Parser(handler);
+
+        parser.write(pageHtml);
+        parser.done();
+
+        const dom = handler.dom;
+
+        // find parent of article content
+        const contentDiv = DomUtils.findOne(elem =>
+                elem.attribs && elem.attribs.class && elem.attribs.class.indexOf("region-content") >=0,
+        dom, true);
+
+        if (!contentDiv) {
+            throw new Error("Article content not found (no 'region-content' class)");
+        }
+
+        // remove article body-enclosing div (class "threed-sidebar-article-body")
+        const bodyDiv = DomUtils.findOne(elem =>
+            elem.attribs && elem.attribs.class && elem.attribs.class.indexOf("threed-sidebar-article-body") >= 0,
+        contentDiv.children, true);
+
+        if (bodyDiv) {
+            const parent: any = bodyDiv.parent;
+            bodyDiv.children.forEach(child => DomUtils.appendChild(parent, child));
+            DomUtils.removeElement(bodyDiv);
+        }
+
+        let imageIndex = 0;
+        const imageUrls: { [id:string]: string } = {};
+
+        DomUtils.findOne(elem => {
+            // download images
+            if (elem.name === "img" && elem.attribs && elem.attribs.src) {
+                const src = elem.attribs.src;
+                const imageUrl = src.startsWith("http") ? src : MigratePlayTask.drupalBaseUrl + src;
+                const imageExtension = imageUrl.split(".").pop();
+                const imageFileName = `article-${index}-image-${imageIndex}.${imageExtension}`;
+
+                elem.attribs.src = imageFileName;
+                imageUrls[imageUrl] = imageFileName;
+                imageIndex++;
+            }
+
+            // remove additional classes from all nodes
+            if (elem.attribs && elem.attribs.class) {
+                delete elem.attribs.class;
+            }
+
+            return false;
+        }, contentDiv.children, true);
+
+        // fetch all images
+        const urls = Object.keys(imageUrls);
+        const promises = urls.map(url => fetch.buffer(url, "GET").then(image => {
+            this.writeFile(imageUrls[url], Buffer.from(image))
+        }));
+
+        // write article HTML content
+        const contentHtml = DomUtils.getInnerHTML(contentDiv);
+        promises.push(this.writeFile(`article-${index}.html`, contentHtml));
+
+        await Promise.all(promises);
     }
 }
