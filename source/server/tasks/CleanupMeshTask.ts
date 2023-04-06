ac96ce9b3a58b61903b0c67e5e213639d894be72
Jamie Cope <jamie@gcope.com>
2023-04-06 13:28:01 -0400

    Added log step to CleanMesh recipe. Fixed parameter bug in Clean task.

--- source/server/tasks/CleanupMeshTask.ts
+++ source/server/tasks/CleanupMeshTask.ts
@@ -88,7 +88,7 @@ export default class CleanupMeshTask extends ToolTask
                 {
                     name: "SelectSmallComponents",
                     params: {
-                        "nbfaceratio": 0.999
+                        "NbFaceRatio": 0.9999
                     }
                 },
                 {
